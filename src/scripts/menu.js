b5ae37d8ff8721abd5f86efe1e09f36e46e9fcc9
Tonie <tonjefiskvik@gmail.com>
2025-09-17 23:02:27 +0200

    fix: div

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,7 @@
+// src/scripts/menu.js
 // Hamburger-meny funksjonalitet
-document.querySelector(".hamburger")?.addEventListener("click", () => {
-  document.querySelector("#menu-list")?.classList.toggle("expanded");
-});
+document
+  .querySelector(".hamburger-container")
+  ?.addEventListener("click", () => {
+    document.querySelector("#menu-list")?.classList.toggle("expanded");
+  });
